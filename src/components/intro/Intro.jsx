c4875b564926b37dd61e167142b7dab637dcc888
Sunriderr37@gmail.com <Timurkrasava1>
2021-07-09 02:08:06 +0300

    added portfolio

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,36 @@
 import './Intro.scss'
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
 export default function Intro() {
+    const textRef = useRef();
+
+    useEffect(() => {
+       init(textRef.current, {
+           showCursor: true,
+           backDelay: 1500,
+           strings: ["Backend and Frontend Developer", "Designer"]
+       })
+    }, [])
+
+
     return (
-        <div className="intro">
-            sdasd
+        <div className="intro" id="intro">
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/man.png" alt="" />
+                </div>
+            </div>
+            <div className="right">
+            <div className="wrapper">
+                <h2>Hi there, my name is</h2>
+                <h1>Timur Utyaganov</h1>
+                <h3>Freelance <span ref={textRef}></span></h3>
+            </div>
+            <a href="#portfolio">
+                <img src="assets/down.png" alt="" />
+            </a>
+            </div>
         </div>
     )
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,8 +2,21 @@ import './Portfolio.scss'
 
 export default function Portfolio() {
     return (
-        <div className="portfolio">
-            dsads 
+        <div className="portfolio" id="portfolio">
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="https://i.pinimg.com/originals/dd/25/c2/dd25c290ab48509b38590fe30ca1c3a9.png" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+            </div>
         </div>
     )
 }
