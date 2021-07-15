6a11138ac52790a69cba322ce629cf8ddc4f2d7f
Sunrider37 <Sunriderr37@gmail.com>
2021-07-15 21:46:00 +0300

    added responsiveness to mobile devices

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,24 +1,56 @@
-import './Testimonials.scss'
+import "./Testimonials.scss";
 
 export default function Testimonials() {
-    return (
-        <div className="testimonials" id="testimonials">
-            <h1>Testimonials</h1>
-            <div className="container">
-                <div className="card">
-                    <div className="top">
-                        <img src="assets/right-arrow.png" className="left" alt="" />
-                        <img className="user" src="https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" />
-                        <img className="right" src="assets/youtube.png" alt="" />
-                    </div>
-                    <div className="center">LOorem ipsum dolor lorem ipsum dolor</div>
-                    <div className="bottom">
-                        <h3>Alex</h3>
-                        <h4>CEO</h4>
-
-                    </div>
-                </div>
+  const data = [
+    {
+      id: 1,
+      name: "Cool Senior Guy",
+      title: "Senior Developer",
+      img: "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/twitter.png",
+      desc: "I don't have any testimonials yet, i just pasted it here so it would look cool",
+    },
+    {
+      id: 2,
+      name: "Alex Rasinski",
+      title: "Co-Founder of DELKA",
+      img: "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/youtube.png",
+      desc: "Let's pretend here's an awesome testimonials written here",
+      featured: true,
+    },
+    {
+      id: 3,
+      name: "Antonio Treloni",
+      title: "CEO of ALBI",
+      img: "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/linkedin.png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+    },
+  ];
+  return (
+    <div className="testimonials" id="testimonials">
+      <h1>Testimonials</h1>
+      <div className="container">
+        {data.map((d) => (
+          <div className={d.featured ? "card featured" : "card"}>
+            <div className="top">
+              <img src="assets/right-arrow.png" className="left" alt="" />
+              <img
+                className="user"
+                src={d.img}
+                alt=""
+              />
+              <img className="right" src={d.icon} alt="" />
             </div>
-        </div>
-    )
+            <div className="center">{d.desc}</div>
+            <div className="bottom">
+              <h3>{d.name}</h3>
+              <h4>{d.title}</h4>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
